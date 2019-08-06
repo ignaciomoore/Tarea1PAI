4716d9dd26c6f056683016431c33eae4911da57a
Ignacio Moore <ignacio.moore@ug.uchile.cl>
2019-08-06 13:40:08 -0400

    argparse is working

--- tarea_1.py
+++ tarea_1.py
@@ -3,10 +3,23 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Encode or Decode an Image')
 parser.add_argument('--image', type=str, help='Image file name')
+parser.add_argument('--text', type=str, help='Text file name')
+parser.add_argument('--nbits', type=int, help='Cantidad de bits')
 group = parser.add_mutually_exclusive_group()
 group.add_argument('--encode', action='store_true', help='Encode text')
 group.add_argument('--decode', action='store_true', help='Decode text')
 args = parser.parse_args()
 
 if __name__ == '__main__':
-    print(args.image)
+    image = args.image
+    if args.encode:
+        print('Encoding')
+        bits = args.nbits
+        text = args.text
+        print(text)
+        print(bits)
+    elif args.decode:
+        print('Decoding')
+    else:
+        print('Somethings wrong')
+    print(image)
