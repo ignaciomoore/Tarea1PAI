9d82e5f64968582692aabd84d9bcfff0771a28df
Ignacio Moore <ignacio.moore@ug.uchile.cl>
2019-08-06 14:10:19 -0400

    it shows de image in the screen

--- tarea_1.py
+++ tarea_1.py
@@ -1,5 +1,8 @@
 
 import argparse
+import skimage.io as skio
+import numpy as np
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description='Encode or Decode an Image')
 parser.add_argument('--image', type=str, help='Image file name')
@@ -9,6 +12,20 @@ group = parser.add_mutually_exclusive_group()
 group.add_argument('--encode', action='store_true', help='Encode text')
 group.add_argument('--decode', action='store_true', help='Decode text')
 args = parser.parse_args()
+
+def to_uint8(image) :
+    if image.dtype == np.float64 :
+        image = image * 255
+    image[image<0]=0
+    image[image>255]=255
+    image = image.astype(np.uint8, copy=False)
+    return image
+
+def imread(filename, as_gray = False):
+    image = skio.imread(filename, as_gray = as_gray)
+    if image.dtype == np.float64:
+        image = to_uint8(image)
+    return image
 
 if __name__ == '__main__':
     image = args.image
@@ -23,3 +40,11 @@ if __name__ == '__main__':
     else:
         print('Somethings wrong')
     print(image)
+
+    picture = imread(image)
+    print('shape: {}'.format(picture.shape))
+
+    plt.imshow(picture, cmap='gray')
+    plt.title(image)
+    plt.axis('off')
+    plt.show()
